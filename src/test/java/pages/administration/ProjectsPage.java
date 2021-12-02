855347a184cb8b9a8e93d9d8423ac2458b5d43a1
veronikazen <Ihaget47>
2021-12-02 20:56:11 +0300

    Corrected some mistakes on variable names

--- src/test/java/pages/administration/ProjectsPage.java
+++ src/test/java/pages/administration/ProjectsPage.java
@@ -9,10 +9,8 @@ import static pages.dashboard.DashboardPage.ACCEPT_ID;
 
 public class ProjectsPage extends BasePage {
 
-    public static final String EDIT_PROJECT_XPATH = "//*[contains(text(), '%s')]//ancestor::*[contains(@class, " +
-            "'hoverSensitive')]//descendant::*[@class='icon-small-edit']";
-    public static final String DELETE_PROJECT_XPATH = "//*[contains(text(), '%s')]//ancestor::*[contains(@class, " +
-            "'hoverSensitive')]//descendant::*[@class='icon-small-delete']";
+    public static final String MANAGE_PROJECT_XPATH = "//*[contains(text(), '%s')]//ancestor::*[contains(@class, " +
+            "'hoverSensitive')]//descendant::*[@class='icon-small-%s']";
     public static final String DELETE_CHECKBOX_XPATH = "//*[@class='dialog-confirm']//child::strong";
 
     public ProjectsPage open() {
@@ -21,14 +19,14 @@ public class ProjectsPage extends BasePage {
     }
 
     public ProjectsPage editProject(String projectName, String editedProjectName) {
-        $x(String.format(EDIT_PROJECT_XPATH, projectName)).click();
+        $x(String.format(MANAGE_PROJECT_XPATH, projectName, "edit")).click();
         new Input("name").clear().write(editedProjectName);
         $(By.id(ACCEPT_ID)).click();
         return this;
     }
 
     public ProjectsPage deleteProject(String projectName) {
-        $x(String.format(DELETE_PROJECT_XPATH, projectName)).click();
+        $x(String.format(MANAGE_PROJECT_XPATH, projectName, "delete")).click();
         $x(DELETE_CHECKBOX_XPATH).click();
         $x(CONFIRM_DELETE_XPATH).click();
         return this;
