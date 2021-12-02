44744719dddd1a80e63ca3c5e9932e6a9cb86cbc
veronikazen <Ihaget47>
2021-12-02 17:45:54 +0300

    Corrected some mistakes on variable names

--- src/test/java/pages/dashboard/DashboardPage.java
+++ src/test/java/pages/dashboard/DashboardPage.java
@@ -1,15 +1,14 @@
 package pages.dashboard;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import org.openqa.selenium.By;
 import pages.BasePage;
+import pages.administration.ProjectsPage;
 import wrappers.Input;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DashboardPage extends BasePage {
 
-    public static final String DASHBOARD_ID= "navigation-dashboard";
     public static final String ADD_PROJECT_ID = "sidebar-projects-add";
     public static final String ACCEPT_ID = "accept";
     public static final String PROJECT_XPATH = "//*[contains(@class, 'summary-auto')]//*[contains(@class, " +
@@ -20,18 +19,13 @@ public class DashboardPage extends BasePage {
         return this;
     }
 
-    public DashboardPage isPageOpened() {
-        $(By.id(DASHBOARD_ID)).shouldBe(Condition.visible);
-        return this;
-    }
-
-    public ProjectDetailsPage createProject(String projectName, String announcement) {
+    public ProjectsPage createProject(String projectName, String announcement) {
         $(By.id(ADD_PROJECT_ID)).click();
         new Input("name").write(projectName);
         new Input("announcement").write(announcement);
         new Input("suite_mode_single").select();
         $(By.id(ACCEPT_ID)).click();
-        return new ProjectDetailsPage();
+        return new ProjectsPage();
     }
 
     public ProjectDetailsPage openProject(String projectName) {
